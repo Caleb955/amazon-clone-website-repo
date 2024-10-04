de76a578aef9c471eb8e4181aad96dd052cca215
Caleb955 <161380361+Caleb955@users.noreply.github.com>
2024-10-04 21:40:55 +0100

    i added a little bit of promise to load the products from the back end before rendering the home page and converted the normal object into a Product object class

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,85 +1,90 @@
-import { products } from "../data/products.js";
+import { products, loadProductFetch } from "../data/products.js";
 import { addToCart, cart } from "../data/cart.js";
-import '../data/cart.js';
 
-let productHTML = '';
+loadProductFetch().then(() => {
+  renderProducts();
+})
 
-products.forEach((product) => {
-    productHTML +=
-    `
-        <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+function renderProducts() {
+  let productHTML = '';
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+  products.forEach((product) => {
+      productHTML +=
+      `
+          <div class="product-container">
+            <div class="product-image-container">
+              <img class="product-image"
+                src="${product.image}">
             </div>
+
+            <div class="product-name limit-text-to-2-lines">
+              ${product.name}
+            </div>
+
+            <div class="product-rating-container">
+              <img class="product-rating-stars"
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
+              <div class="product-rating-count link-primary">
+                ${product.rating.count}
+              </div>
+            </div>
+
+            <div class="product-price">
+              ${product.PriceUrl()}
+            </div>
+
+            <div class="product-quantity-container">
+              <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+              <img src="images/icons/checkmark.png">
+              Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
+              Add to Cart
+            </button>
           </div>
-
-          <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
-          </div>
-
-          <div class="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
-            Add to Cart
-          </button>
-        </div>
-    `;
-});
-
-document.querySelector('.js-products-grid')
-    .innerHTML = productHTML;
-
-const addToCartButton = document.querySelectorAll('.js-add-to-cart-button');
-
-addToCartButton.forEach((button) => {
-    button.addEventListener('click', () => {
-        const {productId} = button.dataset;
-        
-        addToCart(productId);
-
-        updateHeaderQuantity('.js-cart-quantity');
-    });
-});
-
-function updateHeaderQuantity(selector) {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
+      `;
   });
 
-  document.querySelector(selector)
-    .innerHTML = cartQuantity;
+  document.querySelector('.js-products-grid')
+      .innerHTML = productHTML;
+
+  const addToCartButton = document.querySelectorAll('.js-add-to-cart-button');
+
+  addToCartButton.forEach((button) => {
+      button.addEventListener('click', () => {
+          const {productId} = button.dataset;
+          
+          addToCart(productId);
+
+          updateHeaderQuantity('.js-cart-quantity');
+      });
+  });
+
+  function updateHeaderQuantity(selector) {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector(selector)
+      .innerHTML = cartQuantity;
+  }
 }
